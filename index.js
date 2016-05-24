cc38f300d57648016e94fc1ed7c326a8a46272bf
Claire Liu <wenjing.liu09@foxmail.com>
2016-05-24 17:04:27 +0800

    Classes

--- index.js
+++ index.js
@@ -19,3 +19,29 @@ var bob = {
       console.log(this._name + "knows" +f));
   }
 }
+
+class SkinnedMesh extends THREE.Mesh {
+  constructor(geometry, materials){
+    super(geometry, materials);
+
+    this.idMatrix = SkinnedMesh.defaultMatrix();
+    this.bones = [];
+    this.boneMatrices = [];
+  }
+
+  update(camera){
+    super.update();
+  }
+
+  get boneCount(){
+    return this.bones.length;
+  }
+
+  set matrixType(matrixType){
+    this.idMatrix = SkinnedMesh[matrixType]();
+  }
+
+  static defaultMatrix(){
+    return new THREE.Matrix4();
+  }
+}
