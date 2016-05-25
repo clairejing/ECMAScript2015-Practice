4ff350b945fc1499956f84c62367a0acfc6d27a3
Claire Liu <wenjing.liu09@foxmail.com>
2016-05-25 10:49:42 +0800

    destructuring

--- index.js
+++ index.js
@@ -60,5 +60,49 @@ var obj = {
   ['prop_' + (()=>42)()]:42
 }
 
+// Template Strings
 
-// https://github.com/lukehoban/es6features
+//Basic literal string creation
+`In JavaScript '\n' is a line-feed.`
+
+//Multiline strings
+`In JavaScript this is
+not legal.`
+
+//String interpolation
+var name = "Bob", time = "today";
+`Hello ${name}, how are you ${time}?`
+
+//Construct an HTTP request prefix is used to interpret the replacements and construction
+POST `http://foo.org/bar?a=${a}&b=${b}
+    Content-Type: application/json
+    X-Credentials:${credentials}
+   {  "foo": ${foo},
+      "bar":${bar}}`(myOnReadyStateChangeHandler);
+
+//Destructuring
+
+//list matching
+var [a, , b] = [1, 2, 3];
+
+//object matching
+var {op: a, lhs: { op: b}, rhs: c}
+  =getASTNode();
+
+//object matching shorthand
+//binds `op`, `lhs` and `rhs` in scope
+var {op, lhs, rhs} = getASTNode()
+
+// Can be used in parameter position
+function g({name: x}) {
+  console.log(x);
+}
+g({name: 5})
+
+//Fail-soft destructuring
+var [a] = [];
+a === undefined;
+
+//Fail-soft destructing with defaults
+var [a = 1] = [];
+a === 1;
