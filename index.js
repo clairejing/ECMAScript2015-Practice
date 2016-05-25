8defa8fa936c050798f987fbc68500cb8ecaf14b
Claire Liu <wenjing.liu09@foxmail.com>
2016-05-25 14:40:46 +0800

    Symbols

--- index.js
+++ index.js
@@ -236,3 +236,50 @@ wm.size === undefined;
 //Weak Sets
 var ws = new WeakSet();
 wa.add({data: 42});
+
+//Proxies
+
+//Proxying a normal object
+var target = {};
+var handler = {
+  get: function(receiver, name){
+    return `Hello, ${name}!`;
+  }
+}
+
+var p = new Proxy(target, handler);
+p.world === 'Hello, world!';
+
+//Proxing a function object
+var target = function(){
+  return 'I am the target';
+}
+var handler = {
+  apply: function(receiver, ...args){
+    return 'I am the proxy';
+  }
+};
+var p = new Proxy(target, handler);
+p() === 'I am the proxy';
+
+
+//Symbols
+
+var MyClass = (function(){
+  //module scoped symbol
+  var key = Symbol('key');
+
+  function MyClass(privateData){
+    this[key] = privateData;
+  }
+
+  MyClass.prototype = {
+    doStuff: function(){
+
+    }
+  };
+  return MyClass;
+})();
+
+var c = new MyClass('hello');
+c['key'] === undefined;
